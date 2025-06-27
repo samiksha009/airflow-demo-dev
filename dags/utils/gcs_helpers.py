24bcb2541e285a22ec68b0988c3f73f8c325a5ce
samiksha <samiksha.jangid@accenture.com>
2025-06-27 15:50:54 +0900

    add xcom dag

--- dags/utils/gcs_helpers.py
+++ dags/utils/gcs_helpers.py
@@ -3,10 +3,8 @@ import random
 import io
 from faker import Faker
 from google.cloud import storage
-import json
 
-
-def generate_and_upload_sales_data(bucket_name, gcs_path, num_orders=500):
+def generate_and_upload_sales_data(bucket_name, gcs_path, num_orders=500, **kwargs):
     fake = Faker()
     data = {
         "order_id": [i for i in range(1, num_orders + 1)],
@@ -28,4 +26,8 @@ def generate_and_upload_sales_data(bucket_name, gcs_path, num_orders=500):
     bucket = client.bucket(bucket_name)
     blob = bucket.blob(gcs_path)
     blob.upload_from_string(csv_data, content_type="text/csv")
-    print(f"Data uploaded to gs://{bucket_name}/{gcs_path}")
+
+    full_gcs_url = f"gs://{bucket_name}/{gcs_path}"
+    kwargs['ti'].xcom_push(key='gcs_url', value=full_gcs_url)
+
+    print(f"Data uploaded to {full_gcs_url}")
